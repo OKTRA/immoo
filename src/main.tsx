23f091604ac3b9624426533aba3f7eb82ee970a8
oktra <ousmanektra@gmail.com>
2025-05-05 21:16:06 +0000

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TempoDevtools } from "tempo-devtools";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Initialize Tempo Devtools
+TempoDevtools.init();
 
-createRoot(document.getElementById("root")!).render(
-  <App />
-);
+createRoot(document.getElementById("root")!).render(<App />);
